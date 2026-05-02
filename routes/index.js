b0e2a86c9f0dbf1e190a398b9ccf9cf27d3637a5
DahiruAliyuAdamu <dahirualiyuadamu@gmail.com>
2026-05-02 17:50:52 +0100

    Move health-debug to index router

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,14 @@ router.get('/', (req, res) => {
     res.json({ message: 'VTU API Ready' })
 });
 
+router.get('/health-debug', (req, res) => {
+    res.json({
+        ok: true,
+        secretSet: !!process.env.JWT_SECRET,
+        secretLength: process.env.JWT_SECRET?.length,
+        nodeEnv: process.env.NODE_ENV,
+        mongoSet: !!process.env.MONGO_URI
+    });
+});
+
 module.exports = router
