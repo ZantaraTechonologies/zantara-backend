e8e64a5c0918ee2897adf4cd414baafdf5f76a19
DahiruAliyuAdamu <dahirualiyuadamu@gmail.com>
2026-09-10 09:07:16 +0100

    fix(cors): allow PATCH method for payment gateway status activation

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const allowedOrigins = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split('
 app.use(cors({
     origin: allowedOrigins,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
